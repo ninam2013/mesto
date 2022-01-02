a2976c1b95acd1c6c3e24b30e4d829b46e46ccda
Evgenii Manin <ninam2013@gmail.com>
2022-01-02 19:09:28 +0500

    add cards in  array

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-let popupOpenButton = document.querySelector('.profile__button');
-let popupCloseButton = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_js_name');
-let jobInput = document.querySelector('.popup__input_js_job');
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__text');
+const popupOpenButton = document.querySelector('.profile__button');
+const popupCloseButton = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_js_name');
+const jobInput = document.querySelector('.popup__input_js_job');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__text');
 
 function togglePopup() {
   popup.classList.toggle('popup_open');
@@ -28,3 +28,45 @@ function formSubmitHandler(evt) {
 popupOpenButton.addEventListener('click', togglePopup);
 popupCloseButton.addEventListener('click', togglePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+initialCards.forEach((placeData) => {
+
+  const placesContainer = document.querySelector('.places__container');
+  const placeTemplate = document.querySelector('.place-template').content;
+
+  const placeElement = placeTemplate.cloneNode(true);
+  placeElement.querySelector('.place__image').src = placeData.link;
+  placeElement.querySelector('.place__title').textContent = placeData.name;
+
+  placesContainer.append(placeElement);
+});
+
+// closest - поиск снизу вверх
